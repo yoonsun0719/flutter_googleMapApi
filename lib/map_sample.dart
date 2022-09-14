bdac09a92fc7a4c6fbb7f35a0893c2d18af00b8a
yoonsun0719 <dlvtodbstjs@naver.com>
2022-09-14 16:33:00 +0900

    구글지도API

---
+++ lib/map_sample.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapSample extends StatefulWidget {
+  late final String title;
+
+
+  @override
+  _MapSampleState createState() => _MapSampleState();
+}
+
+class _MapSampleState extends State<MapSample> {
+
+  // 애플리케이션에서 지도를 이동하기 위한 컨트롤러
+  late GoogleMapController _controller;
+
+  // 이 값은 지도가 시작될 때 첫 번째 위치입니다.
+  final CameraPosition _initialPosition =
+  CameraPosition(target: LatLng(41.017901, 28.847953));
+
+  // 지도 클릭 시 표시할 장소에 대한 마커 목록
+  final List<Marker> markers = [];
+
+  addMarker(cordinate) {
+    int id = Random().nextInt(100);
+
+    setState(() {
+      markers
+          .add(Marker(position: cordinate, markerId: MarkerId(id.toString())));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: GoogleMap(
+          initialCameraPosition: _initialPosition,
+          mapType: MapType.normal,
+          onMapCreated: (controller) {
+            setState(() {
+              _controller = controller;
+            });
+          },
+          markers: markers.toSet(),
+
+          // 클릭한 위치가 중앙에 표시
+          onTap: (cordinate) {
+            _controller.animateCamera(CameraUpdate.newLatLng(cordinate));
+            addMarker(cordinate);
+          },
+        ),
+
+        // floatingActionButton 클릭시 줌 아웃
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _controller.animateCamera(CameraUpdate.zoomOut());
+          },
+          child: Icon(Icons.zoom_out),
+        ));
+  }
+}
